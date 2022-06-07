d2cd4140adba96122a070644d2b766c7ae61e78f
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-06 21:26:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# variable = 'hello world'
+# print(variable)
+
+name = 'Woodard'
+print(name)
+
+
+# comment
+
+'''
+I am also a comment
+except multi line
+'''
+
+"""
+I, too, am a multi
+line comment
+"""
+
+'''
+Data Types
+
+Text:   str
+Numeric: int, float, complex
+Sequence: list, tuple, range
+Mapping: dict
+Set:   set, frozenset
+Boolean: bool
+Binary: bytes, bytearray, memoryview
+
+'''
